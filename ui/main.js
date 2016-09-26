0100dc11ac544d0d3500efc460710f00ee880bf5
AthulB <athul1998balakrishnan@gmail.com>
2016-09-26 20:37:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,5 +6,5 @@ function marginRight() {
   img.style.marginLeft = marginLeft +'px';
 }
 img.onclick = function() {
-    var interval = setInterval(marginRight,50)
+    var interval = setInterval(marginRight,50);
 };
