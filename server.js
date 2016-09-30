d4a42a707fd3c9e819a3067f84bb7740318440a5
AthulB <athul1998balakrishnan@gmail.com>
2016-09-30 20:17:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ app.get('/counter' , function(req,res){
     res.send(counter.toString());
 });
 
-var names =[];
+var names = [];
 app.get('/sumbit-name', function(req , res) {
     var name =  req.query.name;
     names.push(name);
